bf77704d5837dd827524cd38e2740d4e0d1d698f
Orta Therox <orta.therox@gmail.com>
2016-11-18 13:54:59 +0000

    Shows can show a show

--- source/bot/contexts/shows.js
+++ source/bot/contexts/shows.js
@@ -3,6 +3,10 @@
 import { fbapi } from "../../facebook/api"
 import type { MitosisUser } from "../types"
 import { Cities } from "places"
+import { updateMitosisUser } from "../../db/mongo"
+import { GravityShowsSearchAPI, gravity } from "../artsy-api"
+import { elementForGravityShow } from "./shows/element"
+const querystring = require("querystring")
 
 interface City {
    slug: string,
@@ -15,8 +19,13 @@ interface City {
 // Keys for callback resolution
 
 export const ShowsNearMeKey = "shows-near-me"
+export const ShowsSaveAsMyCity = "shows-save-for-my-city"
+export const ShowsSetMyCity = "shows-set-for-my-city"
 export const ShowsForCityKey = "shows-for-city"
 export const ShowsShowKey = "shows-show"
+export const ShowsShowInfo = "shows-info-show"
+export const ShowsShowArtworks = "shows-artworks-show"
+export const ShowsFavPartner = "shows-favourite-partner"
 
 /**
  * Handles pulling out the payload keys and running the appropriate function
@@ -29,9 +38,11 @@ export const ShowsShowKey = "shows-show"
 export function handleShowsCallbacks(context: MitosisUser, payload: string) {
   if (payload.startsWith(ShowsNearMeKey)) { callbackShowsNearMe(context, payload) }
   if (payload.startsWith(ShowsForCityKey)) { callbackShowsForCity(context, payload) }
+  if (payload.startsWith(ShowsSaveAsMyCity)) { callbackForShowsSaveAsMyCity(context, payload) }
+  if (payload.startsWith(ShowsSetMyCity)) { callbackForSetSaveAsMyCity(context, payload) }
 }
 
-// Shows trending artists
+// Shows a list of shows nearby, or just jumps straight into shows nearby
 async function callbackShowsNearMe(context: MitosisUser, payload: string) {
   fbapi.startTyping(context.fbSenderID)
   const cities = citiesForUser(context)
@@ -42,15 +53,45 @@ async function callbackShowsNearMe(context: MitosisUser, payload: string) {
   }
   // Present a list to choose from
   await fbapi.quickReply(context.fbSenderID, "Which is the closest city to you?", cities.map((city) => {
-    return { content_type: "text", title: city.name, payload: `${ShowsForCityKey}::${city.slug}` }
+    return { content_type: "text", title: city.name, payload: `${ShowsSaveAsMyCity}::${city.slug}::${city.name}` }
   }))
 }
 
-// Get shows for a city
+// Highlight shows for a city
 async function callbackShowsForCity(context: MitosisUser, payload: string) {
-  const [, showCity] = payload.split("::")
+  const [, showCityID] = payload.split("::")
+  const city = Cities.find((c) => c.slug === showCityID)
+  const query = querystring.stringify({
+    near: city.coords.toString(),
+    sort: "-start_at",
+    size: 5,
+    displayable: true,
+    at_a_fair: false
+  })
 
-  fbapi.sendTextMessage(context.fbSenderID, `Works for ${showCity}`)
+  const url = `${GravityShowsSearchAPI}?${query}`
+  const shows = await gravity(url, context)
+  await fbapi.elementCarousel(context.fbSenderID, `Shows near ${city.name}`, shows.map(show => elementForGravityShow(show)), [])
+}
+
+// If you have choosen a city, let it be the default
+async function callbackForShowsSaveAsMyCity(context: MitosisUser, payload: string) {
+  const [, showCityID, cityName] = payload.split("::")
+
+  await fbapi.quickReply(context.fbSenderID, `Would you like to save ${cityName} as your local city?`, [
+    { content_type: "text", title: "Yes please", payload: `${ShowsSetMyCity}::${showCityID}::${cityName}` },
+    { content_type: "text", title: "No thanks", payload: `${ShowsForCityKey}::${showCityID}::${cityName}` }
+  ])
+}
+
+// Save your location if you wanted to, thne show the city
+async function callbackForSetSaveAsMyCity(context: MitosisUser, payload: string) {
+  const [, showCityID, cityName] = payload.split("::")
+  context.artsyLocationCitySlug = showCityID
+  fbapi.startTyping(context.fbSenderID)
+  await updateMitosisUser(context)
+  fbapi.sendTextMessage(context.fbSenderID, `Set ${cityName} as your local city.`)
+  callbackShowsForCity(context, `${ShowsForCityKey}::${showCityID}::${cityName}`)
 }
 
 function citiesForUser(context: MitosisUser): City[] {
